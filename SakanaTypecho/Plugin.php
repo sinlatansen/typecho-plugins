ba6c8e22f84f8e939482d4b2b5b2535507fed59f
lzy <lllzzzyyy@buaa.edu.cn>
2024-02-20 18:26:54 +0800

    新增了sakana话痨功能。 	modified:   SakanaTypecho/Plugin.php

--- SakanaTypecho/Plugin.php
+++ SakanaTypecho/Plugin.php
@@ -9,7 +9,8 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  * @version 1.0.0
  * @link https://shakaianee.top
  */
-class SakanaTypecho_Plugin implements Typecho_Plugin_Interface {
+class SakanaTypecho_Plugin implements Typecho_Plugin_Interface
+{
     private static $jsUrl_local = '/SakanaTypecho/sakana.min.js';
     private static $jsUrl_jsdelivr = 'https://cdn.jsdelivr.net/npm/sakana-widget@2.3.0/lib/sakana.min.js';
     private static $jsUrl_cloudflare = 'https://cdnjs.cloudflare.com/ajax/libs/sakana-widget/2.3.0/sakana.min.js';
@@ -20,7 +21,8 @@ class SakanaTypecho_Plugin implements Typecho_Plugin_Interface {
      * @return void
      * @throws Typecho_Plugin_Exception
      */
-    public static function activate() {
+    public static function activate()
+    {
         // 注册Hook函数
         Typecho_Plugin::factory('Widget_Archive')->header = ['SakanaTypecho_Plugin', 'outputHeader'];
         Typecho_Plugin::factory('Widget_Archive')->footer = ['SakanaTypecho_Plugin', 'outputFooter'];
@@ -34,8 +36,10 @@ class SakanaTypecho_Plugin implements Typecho_Plugin_Interface {
      * @return void
      * @throws Typecho_Plugin_Exception
      */
-    public static function deactivate() {}
-    
+    public static function deactivate()
+    {
+    }
+
     /**
      * 插件配置方法
      *
@@ -43,30 +47,31 @@ class SakanaTypecho_Plugin implements Typecho_Plugin_Interface {
      * @param Typecho_Widget_Helper_Form $form 配置面板
      * @return void
      */
-    public static function config(Typecho_Widget_Helper_Form $form) {
+    public static function config(Typecho_Widget_Helper_Form $form)
+    {
         $cdn_source = new Typecho_Widget_Helper_Form_Element_Radio(
-            'cdn_source', 
+            'cdn_source',
             [
                 'local' => '本地',
                 'jsdelivr' => 'JSDelivr',
                 'cloudflare' => 'CloudFlare'
             ],
-            'local', 
+            'local',
             _t('组件CDN源选择')
         );
         $form->addInput($cdn_source);
 
         $default_character = new Typecho_Widget_Helper_Form_Element_Text(
             'default_character',
-            NULL, 
+            NULL,
             'takina',
-            _t('默认显示的角色'), 
+            _t('默认显示的角色'),
             _t('takina：井之上泷奈</br>chisato：锦木千束</br>详情参考<a href="https://github.com/dsrkafuu/sakana-widget/blob/main/README.zh.md">sakana-widget项目文档</a>')
         );
         $form->addInput($default_character);
 
         $display_setting = new Typecho_Widget_Helper_Form_Element_Checkbox(
-            'display_setting', 
+            'display_setting',
             [
                 'auto_fit' => '自动缩放（没事别关）',
                 'fit_mobile' => '自适应手机端页面（取消边距并缩放）',
@@ -78,19 +83,19 @@ class SakanaTypecho_Plugin implements Typecho_Plugin_Interface {
         $form->addInput($display_setting);
 
         $widget_pos = new Typecho_Widget_Helper_Form_Element_Radio(
-            'widget_pos', 
+            'widget_pos',
             [
                 'left' => '左',
                 'right' => '右'
             ],
-            'left', 
+            'left',
             _t('组件在页面下方的位置')
         );
         $form->addInput($widget_pos);
 
         $widget_size = new Typecho_Widget_Helper_Form_Element_Text(
             'widget_size',
-            NULL, 
+            NULL,
             '200',
             _t('组件大小'),
             _t('输入符合css标准的值（px单位）')
@@ -99,7 +104,7 @@ class SakanaTypecho_Plugin implements Typecho_Plugin_Interface {
 
         $bottom_distance = new Typecho_Widget_Helper_Form_Element_Text(
             'bottom_distance',
-            NULL, 
+            NULL,
             '24',
             _t('底边距'),
             _t('输入符合css标准的值（px单位）')
@@ -108,14 +113,14 @@ class SakanaTypecho_Plugin implements Typecho_Plugin_Interface {
 
         $side_distance = new Typecho_Widget_Helper_Form_Element_Text(
             'side_distance',
-            NULL, 
+            NULL,
             '10',
             _t('侧边距'),
             _t('输入符合css标准的值（px单位）')
         );
         $form->addInput($side_distance->addRule('isFloat', _t('请填写正确的边距！')));
     }
-    
+
     /**
      * 个人用户的配置方法
      *
@@ -123,7 +128,9 @@ class SakanaTypecho_Plugin implements Typecho_Plugin_Interface {
      * @param Typecho_Widget_Helper_Form $form
      * @return void
      */
-    public static function personalConfig(Typecho_Widget_Helper_Form $form) {}
+    public static function personalConfig(Typecho_Widget_Helper_Form $form)
+    {
+    }
 
     /**
      * 插件实现函数 输出header
@@ -133,32 +140,111 @@ class SakanaTypecho_Plugin implements Typecho_Plugin_Interface {
      * @param Widget_Archive $archive
      * @return void
      */
-    public static function outputHeader(string $header, Widget_Archive $archive) {
+    public static function outputHeader(string $header, Widget_Archive $archive)
+    {
         $config = Typecho_Widget::widget('Widget_Options')->Plugin('SakanaTypecho');
 ?>
-<style>
-    #sakana-widget {
-        position: fixed;
-        <?= $config->widget_pos ?>: <?= $config->side_distance ?>px;
-        bottom: <?= $config->bottom_distance ?>px;
-        height: <?= $config->widget_size ?>px;
-        width: <?= $config->widget_size ?>px;
-    }
-<?php 
-    if(@in_array('fit_mobile', $config->display_setting)) {
-    // 手机端的分类讨论处理
-?>
-    @media(max-width: 1024px) {
-        #sakana-widget {
-            bottom: 10px;
-            <?= $config->widget_pos ?>: 0px;
-            height: 120px;
-            width: 120px;
-        }
-    }
-<?php } ?>
-</style>
-<?php
+        <style>
+            #sakana-widget {
+                position: fixed;
+                <?= $config->widget_pos ?>: <?= $config->side_distance ?>px;
+                bottom: <?= $config->bottom_distance ?>px;
+                height: <?= $config->widget_size ?>px;
+                width: <?= $config->widget_size ?>px;
+            }
+
+            #sakana-bubble {
+                position: fixed;
+                bottom: <?= $config->bottom_distance + 215 ?>px;
+                padding: 10px;
+                background-color: #f0f0f0;
+                color: #333;
+                font-size: 1em;
+                border-radius: 10px;
+                /* border: 2px solid #ccc; */
+                filter: drop-shadow(0px 4px 8px rgba(0, 0, 0, 0.2));
+                opacity: 0;
+                /* 初始状态为透明 */
+                transform: translateY(10px);
+                /* 初始状态向下移动10px */
+                transition: opacity 0.5s, transform 0.5s;
+                /* 定义过渡效果 */
+                /* Initially hidden */
+                z-index: 100;
+                /* Ensure it's above other content */
+            }
+
+            #sakana-bubble.show {
+                opacity: 1;
+                /* 显示时完全不透明 */
+                transform: translateY(0);
+                /* 显示时回到原位 */
+            }
+
+            #sakana-bubble::after {
+                content: '';
+                position: fixed;
+                bottom: <?= $config->bottom_distance - 40 ?>px;
+                left: 60px;
+                border-width: 10px;
+                border-style: solid;
+                border-color: #f0f0f0 transparent transparent transparent;
+                /* The first value is the color of the triangle (should match the bubble's background) */
+            }
+
+            <?php
+            if (@in_array('fit_mobile', $config->display_setting)) {
+                // 手机端的分类讨论处理
+            ?>@media(max-width: 1024px) {
+                #sakana-widget {
+                    bottom: 10px;
+                    <?= $config->widget_pos ?>: 0px;
+                    height: 120px;
+                    width: 120px;
+                }
+
+                #sakana-bubble {
+                    position: fixed;
+                    bottom: 150px;
+                    padding: 8px;
+                    background-color: #f0f0f0;
+                    color: #333;
+                    font-size: 0.8em;
+                    border-radius: 10px;
+                    /* border: 2px solid #ccc; */
+                    /* box-shadow: 0px 4px 8px rgba(0, 0, 0, 0.2); */
+                    opacity: 0;
+                    /* 初始状态为透明 */
+                    transform: translateY(10px);
+                    /* 初始状态向下移动10px */
+                    transition: opacity 0.5s, transform 0.5s;
+                    /* 定义过渡效果 */
+                    /* Initially hidden */
+                    z-index: 100;
+                    /* Ensure it's above other content */
+                }
+
+                #sakana-bubble.show {
+                    opacity: 1;
+                    /* 显示时完全不透明 */
+                    transform: translateY(0);
+                    /* 显示时回到原位 */
+                }
+
+                #sakana-bubble::after {
+                    content: '';
+                    position: fixed;
+                    bottom: -18px;
+                    left: 60px;
+                    border-width: 10px;
+                    border-style: solid;
+                    border-color: #f0f0f0 transparent transparent transparent;
+                    /* The first value is the color of the triangle (should match the bubble's background) */
+                }
+
+                <?php } ?>
+        </style>
+    <?php
     }
 
     /**
@@ -168,29 +254,68 @@ class SakanaTypecho_Plugin implements Typecho_Plugin_Interface {
      * @param Widget_Archive $archive
      * @return void
      */
-    public static function outputFooter(Widget_Archive $archive) {
+    public static function outputFooter(Widget_Archive $archive)
+    {
         $config = Typecho_Widget::widget('Widget_Options')->Plugin('SakanaTypecho');
         // CDN源选择，获取js库的url
-        switch($config->cdn_source) {
-            case 'local'     : $jsUrl = Helper::options()->pluginUrl . self::$jsUrl_local;break;
-            case 'jsdelivr'  : $jsUrl = self::$jsUrl_jsdelivr; break;
-            case 'cloudflare': $jsUrl = self::$jsUrl_cloudflare; break;
+        switch ($config->cdn_source) {
+            case 'local':
+                $jsUrl = Helper::options()->pluginUrl . self::$jsUrl_local;
+                break;
+            case 'jsdelivr':
+                $jsUrl = self::$jsUrl_jsdelivr;
+                break;
+            case 'cloudflare':
+                $jsUrl = self::$jsUrl_cloudflare;
+                break;
         }
-?>
-<div id="sakana-widget"></div>
-<script src="<?= $jsUrl ?>"></script>
-<script>
-    window.addEventListener('DOMContentLoaded', () => {
-        new SakanaWidget({
-          character: '<?= $config->default_character ?>',
-          autoFit: <?= @in_array('auto_fit', $config->display_setting) ? 'true':'false' ?>
-        })
-            .mount('#sakana-widget')
-<?php if(@in_array('auto_trigget', $config->display_setting)) {?>
-            .triggetAutoMode()
-<?php } ?>
-      });
-</script>
+        // 对话内容数组
+        $dialogues = json_encode([
+            "Sakana~",
+            "I'm a talking fish!",
+            "最近倒春寒，各位朋友小心感冒~",
+            "网站的所有秘密在那篇置顶文章~",
+            "搜索功能很好用噢，不用慢慢找啦~",
+            "不...不可以扒拉我~",
+            "右下角的蓝色按钮可以返回顶部~"
+        ]);
+    ?>
+        <div id="sakana-widget"></div>
+        <div id="sakana-bubble">Dialogue goes here...</div> <!-- Dialogue bubble element -->
+        <script src="<?= $jsUrl ?>"></script>
+        <script>
+            window.addEventListener('DOMContentLoaded', () => {
+                new SakanaWidget({
+                        character: '<?= $config->default_character ?>',
+                        autoFit: <?= @in_array('auto_fit', $config->display_setting) ? 'true' : 'false' ?>
+                    })
+                    .mount('#sakana-widget')
+                <?php if (@in_array('auto_trigget', $config->display_setting)) { ?>
+                        .triggetAutoMode()
+                <?php } ?>
+                const dialogues = <?= $dialogues ?>;
+                const bubble = document.getElementById('sakana-bubble');
+                let i = 1;
+
+                function showDialogue() {
+                    /* 随机 */
+                    /* const randomIndex = Math.floor(Math.random() * dialogues.length);
+                    bubble.textContent = dialogues[randomIndex]; */
+                    bubble.textContent = '🐟 ' + dialogues[i % dialogues.length];
+                    i = i + 1;
+                    bubble.classList.add('show');
+
+                    /* 一段时间后隐藏气泡，可根据需要调整时间 */
+                    setTimeout(() => {
+                        bubble.classList.remove('show');
+                    }, 5000);
+                }
+
+                showDialogue();
+                /* debugger; */
+                setInterval(showDialogue, 6000);
+            });
+        </script>
 <?php
     }
 }
